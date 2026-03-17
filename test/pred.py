225d11432bc75a64397cbd58f4b2b93ff7775e55
Mikiyas Zenebe <mikilezen@gmail.com>
2026-03-17 08:36:28 -0700

    Modify test samples for prediction function
    Updated test samples in the prediction test function.

--- test/pred.py
+++ test/pred.py
@@ -3,10 +3,7 @@ import requests
 def test_prediction():
     url = "https://disease-classification-1-q58o.onrender.com/predict"
     samples = [
-        "I love this product, it's amazing!",
-        "This is the worst experience ever",
-        "It is okay, nothing special",
-    ]
+        "i have a fever"]
 
     for text in samples:
         response = requests.post(url, json={"text": text})
